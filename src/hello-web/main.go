edb28433a3313fde68dd9a97124e8ff457795563
Moein Torabi <mtorabi.externe@bedrockstreaming.com>
2024-11-17 14:38:04 +0100

    separated my handeler function

--- src/hello-web/main.go
+++ src/hello-web/main.go
@@ -7,21 +7,29 @@ import (
 
 func main() {
     // Define a handler function for the root endpoint
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Query().Get("name")
-		if name != "" {
-			fmt.Fprintf(w, "Hello, %s!", name)
-		} else {
-			fmt.Fprint(w, "Hello, World!")
-		}
-    })
+    http.HandleFunc("/", hander)
 
     // Start the server and listen on port 8080
     fmt.Println("Starting server on port 8080...")
-    
 	err := http.ListenAndServe(":8080", nil)
-	
 	if err != nil {
         fmt.Println("Error starting server:", err)
+    }
+}
+
+func hander(w http.ResponseWriter, r *http.Request) {
+    name := r.URL.Query().Get("name")
+    
+    var message string
+    
+    if name != "" {
+        message = fmt.Sprintf("Hello, %s!", name)
+    } else {
+        message = "Hello, World!"
+    }
+
+    // Write the response and check for errors
+    if _, err := fmt.Fprintf(w, message); err != nil {
+        fmt.Println("Error writing response:", err)
     }
 }
