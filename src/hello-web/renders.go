56bb5c34350ffe438dddece5ba071fb894be9653
Moein Torabi <mtorabi.externe@bedrockstreaming.com>
2024-11-21 23:49:26 +0100

    added caching to the application

--- src/hello-web/renders.go
+++ src/hello-web/renders.go
@@ -4,36 +4,68 @@ import (
     "fmt"
     "net/http"
     "html/template"
+	"path/filepath"
+	"log"
 )
 
-var cachedTmpl = make(map[string]*template.Template)
 
 func RenderTemplate(w http.ResponseWriter, data interface{}, templateFile string) {
 	
-	_, inMap := cachedTmpl[templateFile]
-
-	if !inMap {
-		CreateTemplateCache(templateFile)
+	// Get the cached templates
+	cachedTmpl, err := CreateTemplateCache()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	if err := cachedTmpl[templateFile].Execute(w, data); err != nil {
-		fmt.Println("Error executing template:", err)
+	// Get the cached template
+	parsedTemplates, InMap:= cachedTmpl[templateFile]
+	if !InMap {
+		log.Fatal("cached template not found")
+	}
+
+	// Execute the cached template
+	err_exec := parsedTemplates.Execute(w, data)
+	if err_exec != nil {
+		log.Fatal(err_exec)
 	}
 }
 
-func CreateTemplateCache(templateFile string) error {
-	templates := []string{
-		templateFile,
-		"templates/base.layout.html",
+func CreateTemplateCache() (map[string]*template.Template, error) {
+
+	var cachedTmpl = make(map[string]*template.Template)
+
+	files, err := filepath.Glob("./templates/*.page.html")
+	if err != nil {
+		fmt.Println("Error on files Glob:", err)
+		return cachedTmpl, err
 	}
 
-	tmplParsed, errorParsed := template.ParseFiles(templates...)
-	if errorParsed != nil {
-		return errorParsed
+	for _, file := range files {
+		// Parse the file
+		fileParsed, err := template.ParseFiles(file)
+		if err != nil {
+			fmt.Println("Error parsing files template:", err)
+			return cachedTmpl, err
+		}
+
+		// Get layout files
+		layouts, err := filepath.Glob("./templates/*.layout.html")
+		if err != nil {
+			fmt.Println("Error on layout glob:", err)
+			return cachedTmpl, err
+		}
+
+		// Add layout files to the parsed file
+		if len(layouts) > 0 {
+			fileParsed, err = fileParsed.ParseFiles(layouts...)
+			if err != nil {
+				fmt.Println("Error parsing layout files:", err)
+				return cachedTmpl, err
+			}
+		}
+
+		cachedTmpl[file] = fileParsed
 	}
 
-	cachedTmpl[templateFile] = tmplParsed
-	fmt.Println("Template added to cache:", templateFile)
-
-	return nil
+	return cachedTmpl, nil
 }
